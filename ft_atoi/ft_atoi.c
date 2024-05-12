3407666ecd599bbe55b8d8e6e34325bf9b413025
carlosabreudev <carlosabreudesarrollo@outlook.es>
2024-05-12 16:06:55 +0200

    Funcion atoi y cambios en strnstr

---
+++ ft_atoi/ft_atoi.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * La función convierte la porción inicial de la cadena apuntada por nptr a int.
+ * @param primero cadena que se va a convertir a int
+ * @return el valor convertido a int
+ */
+int ft_atoi(const char *nptr)
+{
+    int num;
+    int sign;
+    num = 0;
+    sign = 1;
+    while (*nptr == ' ')
+        *nptr++;
+    if (*nptr == '-')
+    {
+        sign = -1;
+        nptr++;
+    }
+    else if (*nptr == '+')
+    {
+        nptr++;
+    }
+    while (*nptr >= '0' && *nptr <= '9')
+        num = num * 10 + (*nptr++ - '0');
+    return num * sign;
+}
+
+int main()
+{
+    char num_str[] = " -58 06  9j 1  2345"; // Cadena de caracteres que representa un número
+    int num1;
+    int num2;
+
+    // Usando atoi para convertir la cadena de caracteres a un entero
+    num1 = atoi(num_str);
+    num2 = ft_atoi(num_str);
+
+    // Imprimiendo el resultado
+    printf("El número convertido es: %d\n", num1);
+    printf("El número convertido es: %d\n", num2);
+
+    return 0;
+}
--- ft_srtnstr/ft_strnstr.c
+++ ft_srtnstr/ft_strnstr.c
@@ -17,7 +17,7 @@ int ft_strlen(const char *str)
  * Función que busca una subcadena dentro de otra, según la cantidad de bytes especificados como argumento
  * @param param1 Cadena principal donde se va a buscar
  * @param param2 Subcadena que se va a buscar
- * @param param3 Número máximo de caracteres a comparar dentro de la cadena
+ * @param param3 Número máximo de caracteres a buscar dentro de la cadena
  * @return Un puntero al inicio de la subcadena si la encuentra totalmente, de lo contrario devolverá NULL
  */
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
